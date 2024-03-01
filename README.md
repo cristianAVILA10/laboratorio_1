3bb91c5a4813b55a2c2e4f217a443bd4e4948560
juanca2805 <juan.pedraza2805@gmail.com>
2024-02-29 20:16:26 -0500

    Primer commit

---
+++ README.md
@@ -0,0 +1,18 @@
+
+# Primera aplicacion - Cristian Avila
+
+*Explicacion de funcionamiento de Widget*
+
+Los widget son todos los elementos que se visualizan en la pantalla como botones, ventanas, textos e
+incluso la misma app es un widget
+
+Como componentes principales de nuestra aplicacion de Flutter tenemos:
+
+    - MyAPP: La cual configura la app por completo en donde tendremos la clase MyAppState la cual es la encargada de definir los datos que necesita la app para funcionar, capturando todo cambio o  uso que reciben los witget creados
+
+    - MyHomePage: Es un widget que realiza el seguimiento del estado actual de la app usando el metodo Watch, dento del Witget "MyHomePage" encontraremos todos los botones, textos, ventanas y acciones 
+    de la aplicacion los cuales llamaremos SubWidget
+    
+    - SubWidget: 
+        - SomeWidget:
+        - OtherWidget:
---
+++ lib/main.dart
@@ -0,0 +1,77 @@
+import 'package:english_words/english_words.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (context) => MyAppState(),
+      child: MaterialApp(
+        title: 'Namer App',
+        theme: ThemeData(
+          useMaterial3: true,
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
+        ),
+        home: MyHomePage(),
+      ),
+    );
+  }
+}
+
+class MyAppState extends ChangeNotifier {
+  var current = WordPair.random();
+
+  // ↓ Add this.
+  void getNext() {
+    current = WordPair.random();
+    notifyListeners();
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var appState = context.watch<MyAppState>();
+    var pair = appState.current;
+
+    return Scaffold(
+      body: Column(
+          children: [
+            Text('A random AWESOME idea:'),
+            Tarjeta(appState: appState),
+            Text(pair.asLowerCase),
+            ElevatedButton(
+              onPressed: () {
+                appState.getNext(); // ← This instead of print().
+              },
+              child: Text('Next'),
+            ),
+          ],
+        ),
+      ),
+  }
+}
+
+class Tarjeta extends StatelessWidget {
+  const Tarjeta({
+    super.key,
+    required this.appState,
+  });
+
+  final MyAppState appState;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Text(appState.current.asLowerCase),
+    );
+  }
+}
